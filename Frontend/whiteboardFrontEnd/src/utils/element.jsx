d3b846bb3a145fff72be733382c8d2aac38b9da0
Aatish044 <ms9891128119@gmail.com>
2026-06-24 19:36:06 +0530

    fixed the element

--- Frontend/whiteboardFrontEnd/src/utils/element.jsx
+++ Frontend/whiteboardFrontEnd/src/utils/element.jsx
@@ -25,44 +25,63 @@ export const createElement = (
     stroke,
     size,
   };
+
   let options = {
-    seed: id + 1, // id can't be zero
+    seed: id + 1,
     fillStyle: "solid",
   };
+
   if (stroke) {
     options.stroke = stroke;
   }
+
   if (fill) {
     options.fill = fill;
   }
+
   if (size) {
     options.strokeWidth = size;
   }
+
   switch (type) {
     case TOOL_ITEMS.BRUSH: {
       const brushElement = {
         id,
         points: [{ x: x1, y: y1 }],
-        path: new Path2D(getSvgPathFromStroke(getStroke([{ x: x1, y: y1 }]))),
+        path: new Path2D(
+          getSvgPathFromStroke(getStroke([{ x: x1, y: y1 }]))
+        ),
         type,
         stroke,
       };
       return brushElement;
     }
+
     case TOOL_ITEMS.LINE:
       element.roughEle = gen.line(x1, y1, x2, y2, options);
       return element;
+
     case TOOL_ITEMS.RECTANGLE:
-      element.roughEle = gen.rectangle(x1, y1, x2 - x1, y2 - y1, options);
+      element.roughEle = gen.rectangle(
+        x1,
+        y1,
+        x2 - x1,
+        y2 - y1,
+        options
+      );
       return element;
-    case TOOL_ITEMS.CIRCLE:
-      const cx = (x1 + x2) / 2,
-        cy = (y1 + y2) / 2;
-      const width = x2 - x1,
-        height = y2 - y1;
+
+    case TOOL_ITEMS.CIRCLE: {
+      const cx = (x1 + x2) / 2;
+      const cy = (y1 + y2) / 2;
+      const width = x2 - x1;
+      const height = y2 - y1;
+
       element.roughEle = gen.ellipse(cx, cy, width, height, options);
       return element;
-    case TOOL_ITEMS.ARROW:
+    }
+
+    case TOOL_ITEMS.ARROW: {
       const { x3, y3, x4, y4 } = getArrowHeadsCoordinates(
         x1,
         y1,
@@ -70,6 +89,7 @@ export const createElement = (
         y2,
         ARROW_LENGTH
       );
+
       const points = [
         [x1, y1],
         [x2, y2],
@@ -77,11 +97,15 @@ export const createElement = (
         [x2, y2],
         [x4, y4],
       ];
+
       element.roughEle = gen.linearPath(points, options);
       return element;
+    }
+
     case TOOL_ITEMS.TEXT:
       element.text = "";
       return element;
+
     default:
       throw new Error("Type not recognized");
   }
@@ -90,10 +114,12 @@ export const createElement = (
 export const isPointNearElement = (element, pointX, pointY) => {
   const { x1, y1, x2, y2, type } = element;
   const context = document.getElementById("canvas").getContext("2d");
+
   switch (type) {
     case TOOL_ITEMS.LINE:
     case TOOL_ITEMS.ARROW:
       return isPointCloseToLine(x1, y1, x2, y2, pointX, pointY);
+
     case TOOL_ITEMS.RECTANGLE:
     case TOOL_ITEMS.CIRCLE:
       return (
@@ -102,15 +128,23 @@ export const isPointNearElement = (element, pointX, pointY) => {
         isPointCloseToLine(x2, y2, x1, y2, pointX, pointY) ||
         isPointCloseToLine(x1, y2, x1, y1, pointX, pointY)
       );
-    case TOOL_ITEMS.BRUSH:
-      const elPath = new Path2D(getSvgPathFromStroke(getStroke(element.points)));
+
+    case TOOL_ITEMS.BRUSH: {
+      const elPath = new Path2D(
+        getSvgPathFromStroke(getStroke(element.points))
+      );
       return context.isPointInPath(elPath, pointX, pointY);
-    case TOOL_ITEMS.TEXT:
+    }
+
+    case TOOL_ITEMS.TEXT: {
       context.font = `${element.size}px Caveat`;
       context.fillStyle = element.stroke;
+
       const textWidth = context.measureText(element.text).width;
       const textHeight = parseInt(element.size);
+
       context.restore();
+
       return (
         isPointCloseToLine(x1, y1, x1 + textWidth, y1, pointX, pointY) ||
         isPointCloseToLine(
@@ -129,8 +163,17 @@ export const isPointNearElement = (element, pointX, pointY) => {
           pointX,
           pointY
         ) ||
-        isPointCloseToLine(x1, y1 + textHeight, x1, y1, pointX, pointY)
+        isPointCloseToLine(
+          x1,
+          y1 + textHeight,
+          x1,
+          y1,
+          pointX,
+          pointY
+        )
       );
+    }
+
     default:
       throw new Error("Type not recognized");
   }
